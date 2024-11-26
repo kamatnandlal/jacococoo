f2c110dc05d09447bc901c820aa165006ec5eeb0
tharunraj44 <51993216+tharunraj44@users.noreply.github.com>
2024-11-26 15:54:16 +0530

    Update WorldController.java

--- service2/src/main/java/com/example/service2/WorldController.java
+++ service2/src/main/java/com/example/service2/WorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WorldController {
     @GetMapping("/service2/world")
-    public String sayHello() {
-        return "Hello from Service 2!";
+    public int add(int a, int b) {
+        return a + b;
     }
 }
