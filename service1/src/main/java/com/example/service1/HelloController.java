3eb76cca61470f271f41b5de3665bc7505bea840
tharunraj44 <51993216+tharunraj44@users.noreply.github.com>
2024-11-26 15:30:33 +0530

    Update HelloController.java

--- service1/src/main/java/com/example/service1/HelloController.java
+++ service1/src/main/java/com/example/service1/HelloController.java
@@ -3,7 +3,7 @@ package com.example.service1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/service1/hello")
     public String sayHello() {
