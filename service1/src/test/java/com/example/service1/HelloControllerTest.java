a3e32a19b45e6edffaf3062ececb58d469667144
Nandlal Kamat <38187138+kamatnandlal@users.noreply.github.com>
2024-11-26 10:29:51 +0530

    Update HelloControllerTest.java

--- service1/src/test/java/com/example/service1/HelloControllerTest.java
+++ service1/src/test/java/com/example/service1/HelloControllerTest.java
@@ -24,29 +24,4 @@ public class HelloControllerTest {
                 .andExpect(content().string("Hello from Service 1!"));
     }
 }
-Service 1 Pom File (service1/pom.xml):
 
-xml
-Copy code
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
-    <parent>
-        <groupId>com.example</groupId>
-        <artifactId>microservices-project</artifactId>
-        <version>1.0-SNAPSHOT</version>
-    </parent>
-    <artifactId>service1</artifactId>
-
-    <dependencies>
-        <dependency>
-            <groupId>org.springframework.boot</groupId>
-            <artifactId>spring-boot-starter-web</artifactId>
-        </dependency>
-        <dependency>
-            <groupId>org.springframework.boot</groupId>
-            <artifactId>spring-boot-starter-test</artifactId>
-            <scope>test</scope>
-        </dependency>
-    </dependencies>
-</project>
