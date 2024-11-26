27f8b5820a3b61feb5e86295336c9cae769763a1
tharunraj44 <51993216+tharunraj44@users.noreply.github.com>
2024-11-26 15:43:53 +0530

    Update HelloControllerTest.java

--- service1/src/test/java/com/example/service1/HelloControllerTest.java
+++ service1/src/test/java/com/example/service1/HelloControllerTest.java
@@ -28,7 +28,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class HelloControllerTest {
 
     @Test
     void contextLoads() {
