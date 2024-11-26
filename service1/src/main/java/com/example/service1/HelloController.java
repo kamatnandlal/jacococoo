330aa3901226b714a31cd138a22017638d48fe0c
tharunraj44 <51993216+tharunraj44@users.noreply.github.com>
2024-11-26 16:11:24 +0530

    Update HelloController.java

--- service1/src/main/java/com/example/service1/HelloController.java
+++ service1/src/main/java/com/example/service1/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WorldController {
-    @GetMapping("/service1/Hello")
+    @GetMapping("/service1/hello")
     public String sayHello() {
         return "Hello from Service 1!";
     }
