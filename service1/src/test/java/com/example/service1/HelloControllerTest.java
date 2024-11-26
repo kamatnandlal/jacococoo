d5ef027feea667aa80ebca7fbc170dcb26cda71e
tharunraj44 <51993216+tharunraj44@users.noreply.github.com>
2024-11-26 15:57:39 +0530

    Update HelloControllerTest.java

--- service1/src/test/java/com/example/service1/HelloControllerTest.java
+++ service1/src/test/java/com/example/service1/HelloControllerTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
