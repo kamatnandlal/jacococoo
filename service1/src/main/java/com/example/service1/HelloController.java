5b336968f0004f5ed3f11f5e228f378dcaea01f9
Nandlal Kamat <38187138+kamatnandlal@users.noreply.github.com>
2024-11-26 10:23:02 +0530

    Create HelloController.java

---
+++ service1/src/main/java/com/example/service1/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.service1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/service1/hello")
+    public String sayHello() {
+        return "Hello from Service 1!";
+    }
+}
